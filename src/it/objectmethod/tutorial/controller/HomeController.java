eeed83c31feb6b2a6d97b43dcc4e77d11221117f
Yuri <yuri.poggi@objectmethod.it>
2020-01-21 11:47:36 +0100

    first commit

---
+++ src/it/objectmethod/tutorial/controller/HomeController.java
@@ -0,0 +1,59 @@
+package it.objectmethod.tutorial.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.objectmethod.tutorial.esempio.EsempioSalvo;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	EsempioSalvo variabile;
+	
+	@RequestMapping("/esempio-salvo")
+	public void esempio() {
+//		EsempioSalvo variabile = new EsempioSalvo();
+		System.out.println(variabile.ciaoSalvo());
+	}
+
+//		@RequestMapping("/index")
+//		@RequestMapping(value="/index", method=RequestMethod.GET) 
+	@GetMapping("/index")
+	public String index() {
+		return "home";
+	}
+	
+	@GetMapping("/welcome")
+	public String welcomePage(ModelMap model) {
+		
+		model.addAttribute("benvenuto", "Benvenuto nella pagina di tutorial!!");
+		return "main";
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestParam("username") String username, ModelMap model) {
+		model.addAttribute("user", username);
+		return "forward:/landing-control";
+	}
+	
+	@GetMapping("/pagina/{numeroPagina}/show")
+	public String paginaNumerata(@PathVariable("numeroPagina") Integer numero, ModelMap model) {
+		model.addAttribute("num", numero);
+		return "pagina";
+	}
+	
+	@RequestMapping("/landing-control")
+	public String landingCheck(ModelMap model) {
+		model.addAttribute("passato", "Sono passato da landing!");
+		return "landing";
+	}
+	
+
+}
---
+++ src/it/objectmethod/tutorial/esempio/EsempioSalvo.java
@@ -0,0 +1,12 @@
+package it.objectmethod.tutorial.esempio;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EsempioSalvo {
+
+	public String ciaoSalvo() {
+		return "Ciao Salvo come stai?";
+	}
+	
+}
