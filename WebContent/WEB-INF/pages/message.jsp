a96abaa1a50777792697b848cac3d412f7e5d5fc
Yuri <yuri.poggi@objectmethod.it>
2020-01-27 23:34:59 +0100

    Prepared delete and error message handling

--- WebContent/WEB-INF/pages/message.jsp
+++ WebContent/WEB-INF/pages/message.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,10 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<ul>
-		<c:forEach items="${countries}" var="country">
-			<li><a href="country/${country.code}">${country.name}"</a></li>
-		</c:forEach>
-	</ul>
+	<h3>${message}</h3>
+	<a href="${link}">${linkMessage}"</a>
 </body>
 </html>
--- src/it/objectmethod/tutorial/controller/CountryController.java
+++ src/it/objectmethod/tutorial/controller/CountryController.java
@@ -3,6 +3,7 @@ package it.objectmethod.tutorial.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,11 @@ public class CountryController {
 
 	@GetMapping()
 	public String countryList() {
+		return null;
+	}
+
+	@DeleteMapping()
+	public String deleteCountry() {
 		return null;
 	}
 }
