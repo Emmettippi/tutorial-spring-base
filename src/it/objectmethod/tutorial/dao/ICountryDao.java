555df89fd142d255e55ed1d8ace9d81064f13ca2
debora73 <debora.panaro7@gmail.com>
2020-01-27 14:01:45 +0100

    ciao

--- src/it/objectmethod/tutorial/dao/ICountryDao.java
+++ src/it/objectmethod/tutorial/dao/ICountryDao.java
@@ -3,6 +3,6 @@ package it.objectmethod.tutorial.dao;
 import it.objectmethod.tutorial.model.Country;
 
 public interface ICountryDao {
-
+	//commento
 	public Country getCountryByCode(String code);
 }
