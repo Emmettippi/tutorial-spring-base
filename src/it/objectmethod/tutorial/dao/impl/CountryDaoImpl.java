273ba9baff2267162156326d47aae8fb7dffad21
Oxisian <andreamanna50@gmail.com>
2020-01-27 17:55:16 +0100

    WIP: tutorial JDBC

--- src/it/objectmethod/tutorial/dao/impl/CountryDaoImpl.java
+++ src/it/objectmethod/tutorial/dao/impl/CountryDaoImpl.java
@@ -1,28 +1,23 @@
 package it.objectmethod.tutorial.dao.impl;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 
 import it.objectmethod.tutorial.dao.ICountryDao;
 import it.objectmethod.tutorial.model.Country;
-import it.objectmethod.tutorial.model.mapper.CountryMapper;
 
-public class CountryDaoImpl implements ICountryDao {
-
-	private DataSource dataSource;
-	private JdbcTemplate jdbcTemplateObject;
-
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
-	}
+public class CountryDaoImpl extends NamedParameterJdbcDaoSupport implements ICountryDao {
 
 	@Override
 	public Country getCountryByCode(String code) {
-		Country country = null;
+		
 		String sql = "SELECT * FROM country WHERE code = ?";
-		country = this.jdbcTemplateObject.queryForObject(sql, new Object[] { code }, new CountryMapper());
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("code", code);
+		BeanPropertyRowMapper<Country> rm =
+				new BeanPropertyRowMapper<Country>(Country.class);
+		Country country = getNamedParameterJdbcTemplate().queryForObject(sql, params, rm);
 
 		// Better implementation
 		//	String sql = "SELECT code codice, name nome FROM country WHERE code = :codiceInserito";
@@ -33,5 +28,28 @@ public class CountryDaoImpl implements ICountryDao {
 
 		return country;
 	}
+	
+	@Override
+	public Integer getMaxCountryId() {
+		String sql ="SELECT MAX (idcountry) AS maximum FROM country";
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		BeanPropertyRowMapper<Integer> rm =	new BeanPropertyRowMapper<Integer>(Integer.class);
+		int max = getNamedParameterJdbcTemplate().queryForObject(sql, params, Integer.class);
+		return max;
+	}
 
+	@Override
+	public Country insertCountry(Country country) {
+		String sql = "INSERT INTO country (idcountry, code, name) VALUES (:idcountry, :code, :name)";
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("idcountry", getMaxCountryId()+1);
+		params.addValue("code", country.getCodice());
+		params.addValue("name", country.getNome());
+		BeanPropertyRowMapper<Country> rm =
+		new BeanPropertyRowMapper<Country>(Country.class);
+		country = getNamedParameterJdbcTemplate().queryForObject(sql, params, rm);
+		return country;
+	}
+	
+	
 }
